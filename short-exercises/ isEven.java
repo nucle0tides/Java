fcca3209ef219d5e5928ec12e3c142b7fbeb57b5
Gabby <ortmangm@iastate.edu>
2015-02-08 19:30:05 -0600

    Create  isEven.java

---
+++ short-exercises/ isEven.java
@@ -0,0 +1,18 @@
+package isEven;
+//Playing with the Mod operator and print stream.
+
+// import java.util.Scanner;
+// update this when you know what to do with scanners? 
+
+public class isEven {
+	public static void main(String[] args) { 
+		int num = 500 ; 
+			if (num % 2 == 0) { 
+				System.out.println("This number is even."); 
+			}
+			else { 
+				System.out.println("This number is odd."); 
+			}
+		
+	}
+}
