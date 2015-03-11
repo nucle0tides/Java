7db90579534a44c4d72bea703c63a6a47e97d919
Gabby <ortmangm@iastate.edu>
2015-03-10 20:58:20 -0500

    hip update. now with the correct range.

--- short-exercises/Dice.java
+++ short-exercises/Dice.java
@@ -1,11 +1,11 @@
-package randomNum;
+package smallExercises;
 import java.util.*;
-//A poor dice rolling simulator. It prints out a random number from 0-6.
+//A poor dice rolling simulator. It prints out a random number from 1-6.
 
 public class Dice {
 	public static void main(String[] args){ 
 		Random generator = new Random(); 
-		int value = generator.nextInt(7); 
+		int value = generator.nextInt(6) + 1; 
 		//okay, but what about a range that doesn't include 0? 
 		System.out.println("The random number is " + value + "."); 
 	}
